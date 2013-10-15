5d6bc129a6823135ebb17529e9eebcfbd191bef2
Alice Ferrazzi <alice.ferrazzi@gmail.com>
2013-10-15 17:58:31 +0900

    added travi.yaml and pyherd_test

--- test/pyherd_test.py
+++ test/pyherd_test.py
@@ -1,1 +1,15 @@
-init
+import unittest
+import pyherd
+
+class Test(unittest.TestCase):
+    """Unit tests for googlemaps."""
+
+    def test_local_search(self):
+        """Test pyherd search()."""
+        herd = pyherd.herd()
+        run = herd.run("/usr/portage/", "foo/foo", "/metadata.xml")
+        result = run['responseData']['results'][0]
+        self.assertEqual(result['titleNoFormatting'], 'grobian@gentoo.orgnet-mail@gentoo.org')
+
+if __name__ == "__main__":
+    unittest.main()
